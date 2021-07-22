92411676decfb0cc9ec5bb50f62ff7b9a40b15c3
ROUSSEAU Christopher <c.rousseau36@gmail.com>
2021-07-22 16:59:23 +0200

    testing login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -12,10 +13,11 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
+
         // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
+        //     return $this->json("C'est good !", 200);
         // }
 
         // get the login error if there is one
@@ -23,8 +25,8 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-        //return $this->json("L'adresse mail ou le mot de passe est incorrect", 401);
+        // return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->json("L'adresse mail ou le mot de passe est incorrect", 401);
     }
 
     /**
