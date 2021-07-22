ea0948a03e1238c6058208121b9c6e84f1b84c3d
ROUSSEAU Christopher <c.rousseau36@gmail.com>
2021-07-22 14:48:34 +0200

    Fix JWT Token

--- src/Entity/ReviewPill.php
+++ src/Entity/ReviewPill.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\ReviewPillRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ReviewPillRepository::class)
