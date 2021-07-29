0e14bace4dd2d2ae1d53aa3e98ee24f112e4a44e
Lulu Bouron <lce.bouron@gmail.com>
2021-07-29 14:05:37 +0200

    email confirmation for review validation and score update

--- src/Controller/Admin/ReviewPillController.php
+++ src/Controller/Admin/ReviewPillController.php
@@ -7,6 +7,8 @@ use App\Entity\ReviewPill;
 use App\Repository\PillRepository;
 use App\Repository\ReviewPillRepository;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -76,10 +78,12 @@ class ReviewPillController extends AbstractController
      * Method to validate one review (will go from status : 0 to status:1)
      * @Route("/{id}/validate/", name="validate", methods="GET", requirements={"id"="\d+"})
      */
-    public function validateReview(ReviewPill $review, PillRepository $pill): Response
+    public function validateReview(ReviewPill $review, PillRepository $pill, MailerInterface $mailer): Response
     {
         // change the status to 1 when clicked on validated,
         $reviewValidate = $review->setStatus(1);
+
+        // get the current score given in the Review entity
 
         $reviewAcne = $review->getAcne();
         $reviewLibido = $review->getLibido();
@@ -89,22 +93,33 @@ class ReviewPillController extends AbstractController
         $reviewNausea = $review->getNausea();
         $reviewPms = $review->getPms();
 
-        
+        // get the current score linked to the Pill entity stored in the DB
         $pill = $review->getPill();
-        
-        // change the score (increment) in the pill entity
 
         $scoreAcne = $pill->getScoreAcne();
+        $scoreLibido = $pill->getScoreLibido();
+        $scoreMigraine = $pill->getScoreMigraine();
+        $scoreWeight = $pill->getScoreWeight();
+        $scoreBreastPain = $pill->getScoreBreastPain();
+        $scoreNausea = $pill->getScoreNausea();
+        $scorePms = $pill->getScorePms();
 
-        // change the score (increment) in the pill entity
+        // change the score (by adding the current score of the pill to the score given in the review) in the pill entity
         $pillScoreAcne = $pill->setScoreAcne($reviewAcne + $scoreAcne);
+        $pillScoreLibido = $pill->setScoreLibido($reviewLibido + $scoreLibido);
+        $pillScoreMigraine = $pill->setScoreMigraine($reviewMigraine + $scoreMigraine);
+        $pillScoreWeight = $pill->setScoreWeight($reviewWeight + $scoreWeight);
+        $pillScoreBreastPain = $pill->setScoreBreastPain($reviewBreastPain + $scoreBreastPain);
+        $pillScoreNausea = $pill->setScoreNausea($reviewNausea + $scoreNausea );
+        $pillScorePms = $pill->setScorePms($reviewPms + $scorePms);
         
-        $pillScoreLibido = $pill->setScoreLibido($reviewLibido + $reviewLibido);
-        $pillScoreMigraine = $pill->setScoreMigraine($reviewMigraine + $reviewMigraine);
-        $pillScoreWeight = $pill->setScoreWeight($reviewWeight + $reviewWeight);
-        $pillScoreBreastPain = $pill->setScoreBreastPain($reviewBreastPain + $reviewBreastPain);
-        $pillScoreNausea = $pill->setScoreNausea($reviewNausea + $reviewNausea );
-        $pillScorePms = $pill->setScorePms($reviewPms + $reviewPms);
+        $email = (new Email())
+        ->from('no-reply@maestra.fr')
+        ->to('maestra@chrisdev.fr')
+        ->subject('Avis validé sur Mestra.fr ♥')
+        ->text('Bonjour ' . 'Votre avis pour la pilule a bien été validé ! Merci beaucoup pour ton retour !');
+
+        $mailer->send($email);
         
         $em = $this->getDoctrine()->getManager();
         $em->persist($reviewValidate);
@@ -116,7 +131,7 @@ class ReviewPillController extends AbstractController
         $em->persist($pillScoreNausea);
         $em->persist($pillScorePms);
         $em->flush();
-
+       
         $this->addFlash(
             'success',
             'L\'avis à bien été validé !'
