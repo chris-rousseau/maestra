57353aadbca334c9d349c5b808f90a5d89f59f04
ROUSSEAU Christopher <c.rousseau36@gmail.com>
2021-08-03 16:30:23 +0200

    Tests for User

--- tests/Admin/UserTest.php
+++ tests/Admin/UserTest.php
@@ -7,16 +7,79 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserTest extends WebTestCase
 {
-    public function connexionBackOffice(): void
+    public function testConnexionBackOfficeLoggedInAdmin(): void
     {
         $client = static::createClient();
+        // Simulating a connexion with a role admin (ROLE_ADMIN) 
         $userRepository = static::getContainer()->get(UserRepository::class);
-
         $testUser = $userRepository->findOneBy(['email' => 'chris@gmail.com']);
 
         $client->loginUser($testUser);
 
+        // Testing and asserting that as an ADMIN, we have access to the page
         $client->request('GET', '/admin');
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testUserListUnAuthorized()
+    {
+        $client = static::createClient();
+
+        // Simulating a connexion with a role moderator (ROLE_MODERATOR)        
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneByEmail('lulu@boubou.fr');
+        $client->loginUser($user);
+
+        // Testing and asserting that a user with a ROLE_MODERATOR doesn't have
+        // access to the admin page listing the users (Error 403)
+        $crawler = $client->request('GET', '/admin/user');
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function testUserListAuthorized()
+    {
+        $client = static::createClient();
+
+        // Simulating a connexion with a role administrator (ROLE_ADMIN)        
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneByEmail('chris@gmail.com');
+        $client->loginUser($user);
+
+        // Testing and asserting that as an ADMIN, we have access to the page
+        $crawler = $client->request('GET', '/admin/user');
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testUpgradeUserInModerator()
+    {
+        $client = static::createClient();
+
+        // Simulating a connexion with a role administrator (ROLE_ADMIN)        
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneByEmail('chris@gmail.com');
+        $client->loginUser($user);
+
+        $crawler = $client->request('GET', '/admin/user');
+
+        $crawler->selectLink('Passer modérateur')->link();
+
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testUpgradeUserInAdmin()
+    {
+        $client = static::createClient();
+
+        // Simulating a connexion with a role administrator (ROLE_ADMIN)        
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneByEmail('chris@gmail.com');
+        $client->loginUser($user);
+
+        $crawler = $client->request('GET', '/admin/user');
+
+        $crawler->selectLink('Passer admin')->link();
+
 
         $this->assertResponseIsSuccessful();
     }
